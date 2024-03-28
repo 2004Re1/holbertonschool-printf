5d406bbbc9ec53ad5769bd8b4b69808497b7382f
Re1 <118055755+2004Re1@users.noreply.github.com>
2024-03-28 16:36:09 +0400

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -12,7 +12,7 @@ int _printf(const char *format, ...)
     va_start(lol, format);
 
     
-    for(i = 0,len = 0; format[i] != '\0' ; i++)
+    for(i = 0 ; format[i] != '\0' ; i++)
     {
     if(format[i] != '%')
     {
