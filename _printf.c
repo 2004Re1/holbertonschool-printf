f8737f51f2f75fbc6e864fb8317f4baf341ec0f5
kanangoylarov <112507822+kanangoylarov@users.noreply.github.com>
2024-03-29 13:46:25 +0400

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -24,45 +24,6 @@ int _printf(const char *format, ...)
         _putchar(format[i]);
         continue;
    
-    }   
-    if (format[i+1] == 'd'|| format[i+1] == 'i') 
-    {
-	    int x = va_arg(lol,int);
-	    int len = 0;
-	    char *p;
-	
-	    int isNegative = 0;
-	    if (x < 0) {
-	        isNegative = 1;
-	        x = -x;
-	        len++;
-	    }
-	
-	    int temp = x;
-	    do {
-	        len++;
-	        temp /= 10;
-	    } while (temp != 0);
-	
-	   
-	    p = (char*)malloc((len + 1) * sizeof(char)); // +1 for null terminator
-	
-	
-	    int i = len - 1;
-	    do {
-	        p[i--] = (x % 10) + '0';
-	        x /= 10;
-	    } while (x != 0);
-	
-	
-	    if (isNegative)
-	        p[0] = '-';
-	
-	 
-	    p[len] = '\0';
-	
-	    _puts(p);
-	    free(p);
     }
     
     else if (format[i+1] == 's')
