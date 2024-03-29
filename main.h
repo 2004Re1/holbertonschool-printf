f36bbe730bf527e48511976297eba2a46ff7a6a6
Re1 <118055755+2004Re1@users.noreply.github.com>
2024-03-29 17:17:22 +0400

    Update main.h

--- main.h
+++ main.h
@@ -5,6 +5,6 @@ int _putchar(char c, int *counter);
 void _puts(char *str, int *counter);
 int _printf(const char *format, ...);
 void checker (const char *format,va_list *lol, int *counter);
-void _printnum(int x);
+void _printnum(int x, int *counter);
 
 #endif
