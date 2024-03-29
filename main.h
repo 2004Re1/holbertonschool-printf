16d62795366fe0d71fdea9b611ec54310ca49cc9
Re1 <118055755+2004Re1@users.noreply.github.com>
2024-03-29 17:16:11 +0400

    Update main.h

--- main.h
+++ main.h
@@ -1,9 +1,10 @@
 #ifndef HEADER
 #define HEADER
 
-int _putchar(char c);
-void _puts(char *str);
+int _putchar(char c, int *counter);
+void _puts(char *str, int *counter);
 int _printf(const char *format, ...);
+void checker (const char *format,va_list *lol, int *counter);
 void _printnum(int x);
 
 #endif
