0ccd8eb382abcc29469ba862a50384e5e60cc130
kanangoylarov <goylerov03@gmail.com>
2024-03-30 09:17:24 +0000

    my_cmmits

--- main.h
+++ main.h
@@ -4,9 +4,9 @@
 int _putchar(char c, int *counter);
 void _puts(char *str, int *counter);
 int _printf(const char *format, ...);
-void checker (char flag, va_list *lol, int *counter);
+void checker(char flag, va_list *lol, int *counter);
 void _printnum(int x, int *counter);
-void _number_len(int x,int *len);
-void Negative_check(int *x,int *isNegative, int *len);
+void _number_len(int x, int *len);
+void Negative_check(int *x, int *isNegative, int *len);
 
 #endif
