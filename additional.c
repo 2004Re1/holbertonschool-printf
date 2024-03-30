2043422005c4d02c53278395403f2f2c2968e127
Re1 <118055755+2004Re1@users.noreply.github.com>
2024-03-30 11:52:42 +0400

    Update additional.c

--- additional.c
+++ additional.c
@@ -31,26 +31,36 @@ void _puts(char *str,int *counter)
 	}
 }
 
-void _printnum(int x,int *counter)
+void _number_len(int x,int *len)
 {
     int temp;
+    temp = x;
+    do {
+        (*len)++;
+        temp /= 10;
+    } while (temp != 0);
+}
+
+void Negative_check(int *x,int *isNegative, int *len)
+{
+     if (*x < 0) {
+        *isNegative = 1;
+        (*len)++;
+        if (*x != INT_MIN)
+        {
+            *x = -(*x);
+        }
+    }
+}
+
+void _printnum(int x, int *counter)
+{
     int i;
     int len = 0;
     char *p;
     int isNegative = 0;
-    if (x < 0) {
-        isNegative = 1;
-        len++;
-        if (x != INT_MIN)
-        {
-            x = -x;
-        }
-    }
-    temp = x;
-    do {
-        len++;
-        temp /= 10;
-    } while (temp != 0);
+    Negative_check(&x,&isNegative,&len);
+    _number_len(x,&len);
     p = (char*)malloc((len + 1) * sizeof(char));
     if (p == NULL)
     {
@@ -74,6 +84,6 @@ void _printnum(int x,int *counter)
         p[0] = '-';
     }
     p[len] = '\0';
-    _puts(p,counter);
+    _puts(p, counter);
     free(p);
 }
